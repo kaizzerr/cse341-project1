5bd495c7f6b8165f4bd860b6cbd54863234f74d3
kaizzerr <kaizzergatan@gmail.com>
2025-09-17 21:56:21 -0600

    Swagger Correction 2: Project 1 Lesson 6

--- controllers/users.js
+++ controllers/users.js
@@ -23,10 +23,11 @@ const getSingle = async (req, res) => {
 const createUser = async (req, res) => {
   //#swagger.tags=['Users']
     const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
         email: req.body.email,
-        username: req.body.username,
-        name: req.body.name,
-        ipaddress: req.body.ipaddress
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
       };
       const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
       if (response.acknowledged) {
@@ -40,10 +41,11 @@ const updateUser = async (req, res) => {
   //#swagger.tags=['Users']
     const userId = new ObjectId(req.params.id);
     const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
         email: req.body.email,
-        username: req.body.username,
-        name: req.body.name,
-        ipaddress: req.body.ipaddress
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
     };
     const response = await mongodb.getDatabase().db().collection('users').replaceOne({ _id: userId }, user);
     if (response.modifiedCount > 0) {
